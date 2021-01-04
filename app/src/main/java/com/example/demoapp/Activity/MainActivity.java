2f7fbc39c3980cc16ea3ca10611ac7c298a9c7c9
Amit Vijaranyia <amitvijaranyia@gmail.com>
2021-01-04 09:36:34 +0530

    no changes

--- app/src/main/java/com/example/demoapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/demoapp/Activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.demoapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ActionBar;
 import android.graphics.Color;
@@ -12,21 +13,50 @@ import android.view.WindowManager;
 
 import com.example.demoapp.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvVideos;
+    VideosAdapter videosAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.WHITE);
         }
-
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setNavigationBarColor(getResources().getColor(R.color.app_color));
         }
+
+        rvVideos = findViewById(R.id.rvVideos);
+
+
+        makeHttpRequest();
+    }
+
+    public void makeHttpRequest(){
+        String url = "http://takatakapp.xyz/API/index.php?p=showAllVideos";
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("fb_id", "0");
+            jsonObject.put("token", "eynBsaVYwIE:APA91bEmLkA0mold83uPz1N570IZjPGwAUE_o93EkDCPjSZ5-sQRxKRRwAEsSpiGvEOFMq06XQxrCx0k1Kbh8GVjYr3OmDjSGikCiJCReVGsM4-hHvPWgYMOaFrP-9HaO1JfWDg7PgZV");
+            jsonObject.put("type", "related");
+            jsonObject.put("page", "1");
+            jsonObject.put("device_id", "af37ba10f52bca24");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        OkHttpClient client = new OkHttpClient();
+
     }
 }
